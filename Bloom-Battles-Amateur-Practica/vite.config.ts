3cbc5f9fe740de45fe89a7d9e3e2de01dc60edcc
Jdsolano02 <jdsolano03@gmail.com>
2024-09-19 01:20:21 -0600

    changes again

--- Bloom-Battles-Amateur-Practica/vite.config.ts
+++ Bloom-Battles-Amateur-Practica/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BBAP/",
+  base: "/<BBAP>/", // Replace <BBAP> with your repository name if necessary
 });
